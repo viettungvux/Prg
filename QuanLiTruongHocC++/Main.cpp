f29e04830e47ddbdbafde6243c58cca70314ec28
= <=>
2021-01-12 19:50:34 +0700

    Add menu

--- QuanLiTruongHocC++/Main.cpp
+++ QuanLiTruongHocC++/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 class Person {
 private:
@@ -124,10 +125,13 @@ public:
 		cout << "Khoa luan: " << this->khoaLuan << endl;
 	}
 };
-static class CanBo :public Person {
+class CanBo :public Person {
 private:
 	string khoa;
 public:
+	CanBo() {
+
+	}
 	string Khoa() {
 		return this->khoa;
 	}
@@ -143,18 +147,12 @@ public:
 		Person::ShowInfo();
 		cout << "Khoa: " << this->khoa << endl;
 	}
-	double virtual TinhLuong() {
-		return 1;
-	}
+	double virtual TinhLuong() = 0;
 };
 class DayHoc {
 public:
-	double virtual ChamDiem(double mark, double chuyenCan) {
-		return mark * 1 + chuyenCan * 1;
-	}
-	void virtual KiemTraGiuaKi() {
-
-	}
+	double virtual ChamDiem(double mark, double chuyenCan) = 0;
+	void virtual KiemTraGiuaKi() = 0;
 };
 class GiangVienBienChe :public CanBo, DayHoc {
 private:
@@ -176,7 +174,7 @@ public:
 		cout << "He so luong: " << this->heSoLuong << endl;
 	}
 	double TinhLuong() {
-		return this->heSoLuong * 5000000 + 200000;
+		return this->heSoLuong * 5000 + 200;
 	}
 	double ChamDiem(double mark, double chuyenCan) {
 		return mark * 0.6 + chuyenCan * 0.4;
@@ -206,7 +204,7 @@ public:
 
 	}
 	double TinhLuong() {
-		return this->soTietDay*250000;
+		return this->soTietDay * 1.0* 250;
 	}
 	double ChamDiem(double mark, double chuyenCan) {
 		return mark * 0.7 + chuyenCan * 0.3;
@@ -216,5 +214,143 @@ public:
 	}
 };
 int main() {
+	vector<SinhVien*> lop;
+	GiangVienBienChe* gvbc = new GiangVienBienChe();
+	GiangVienHopDong* gvhd = new GiangVienHopDong();
+	int flag = 0;
+	int key;
+	do {
+		cout << "=====================&Manage School&=====================" << endl;
+		cout << "1. Nhap thong tin giao vien" << endl;
+		cout << "2. Xem thong tin giang vien" << endl;
+		cout << "3. Nhap thong tin sinh vien" << endl;
+		cout << "4. Xuat thong tin sinh vien cua lop" << endl;
+		cout << "5. Tim kiem sinh vien theo ten" << endl;
+		cout << "6. In diem trung binh cua cac sinh vine" << endl;
+		cout << "0. Thoat" << endl;
+		cout << "=====================&Manage School&=====================" << endl;
+		cout << "Chon chuc nang: ";
+		cin >> key;
+		switch (key) {
+		case 0:
+			break;
+		case 1: {
+			int func = 0;
+			do {
+				system("cls");
+				cout << "==============&Quan li Giang vien&=============" << endl;
+				cout << "1. Giao vien bien che" << endl;
+				cout << "2. Giao vien hop dong" << endl;
+				cout << "0. Thoat" << endl;
+				cout << "==============&Quan li Giang vien&=============" << endl;
+				cout << "Chon giao vien: ";
+				cin >> func;
+				switch (func) {
+				case 0:
+					break;
+				case 1: {
+					flag = 0;
+					gvbc->Input();
+					break;
+				}
+				case 2: {
+					flag = 1;
+					gvhd->Input();
+					break;
+				}
+				default:
+					cout << "Chuc nang khong hop le" << endl;
+					break;
+				}
+			} while (func != 0);
+			system("cls");
+			break;
+		}
+		case 2:
+			if (flag == 0) {
+				gvbc->ShowInfo();
+				cout << "Luong: " << gvbc->TinhLuong() << endl;
+			}
+			else {
+				gvhd->ShowInfo();
+				cout << "Luong: " << gvhd->TinhLuong() << endl;
+			}
+			break;
+		case 3: {
+			system("cls");
+			int func = 0;
+			SinhVien* sv;
+			do {
+				system("cls");
+				cout << "==============&Quan li Sinh vien&=============" << endl;
+				cout << "1. Sinh vien kinh te" << endl;
+				cout << "2. Sinh vien cntt" << endl;
+				cout << "0. Thoat" << endl;
+				cout << "==============&Quan li Sinh vien&=============" << endl;
+				cout << "Chon sinh vien: ";
+				cin >> func;
+				switch (func) {
+				case 0:
+					break;
+				case 1:
+					sv = new SinhVienKinhTe();
+					sv->Input();
+					lop.push_back(sv);
+					break;
+				case 2:
+					sv = new SinhVienCNTT();
+					sv->Input();
+					lop.push_back(sv);
+					break;
+				default:
+					cout << "Chuc nang khong hop le" << endl;
+					break;
+				}
+			} while (func != 0);
+			system("cls");
+			break;
+		}
+		case 4:
+			cout << "Thong tin cac sinh vien" << endl;
+			for (int i = 0; i < lop.size(); i++) {
+				lop.at(i)->ShowInfo();
+			}
+			break;
+		case 5: {
+			string name;
+			cout << "Nhap ten sinh vien muon tim: ";
+			cin.ignore();
+			getline(cin, name);
+			int mark = 0;
+			for (int i = 0; i < lop.size(); i++) {
+				if (lop.at(i)->Name() == name) {
+					lop.at(i)->ShowInfo();
+					mark = 1;
+				}
+			}
+			if (mark == 0) {
+				cout << "Khong tim thay sinh vien nao co ten " << name << endl;
+			}
+			break;
+		}	
+		case 6: {
+			for (int i = 0; i < lop.size(); i++) {
+				if (flag == 0) {
+					cout << "Ten: " << lop.at(i)->Name() << "----------Diem: "<< gvbc->ChamDiem(lop.at(i)->Mark(), lop.at(i)->ChuyenCan()); 
+					cout<< endl;
+				}
+				else {
+					cout << "Ten: " << lop.at(i)->Name() << "----------Diem: "<<gvhd->ChamDiem(lop.at(i)->Mark(), lop.at(i)->ChuyenCan());
+					cout<< endl;
+				}
+			}
+			break;
+		}
+		default:
+			cout << "Chuc nang khong hop le" << endl;
+			break;
+		}
+		//system("cls");
+	} while (key != 0);
 	return 0;
 }
